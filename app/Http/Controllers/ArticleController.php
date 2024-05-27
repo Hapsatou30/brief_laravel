78a5d566966cab4bdc9105dd5b95689ab2f7dbf2
Haps <thiamhapstou@gmail.com>
2024-05-27 13:02:42 +0000

    Ajout de la fonctionnalité ajouter

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -12,4 +12,25 @@ class ArticleController extends Controller
         $articles = Article::all();
         return view('articles', compact('articles'));
     }
+    public function AddArticle(){
+        return view('/Add');
+    }
+    public function traitementAdd(Request $request)
+    {
+        $request->validate([
+            'nom'=>'required',
+            'description'=>'required',
+            'date_creation'=>'required',
+            'la_une'=>'nullable|boolean',
+            'image'=>'required',
+        ]);
+        $article = new Article();
+        $article->nom = $request->nom;
+        $article->description = $request->description;
+        $article->date_creation = $request->date_creation;
+        $article->la_une = $request->la_une;
+        $article->image = $request->image;
+        $article->save();
+        return redirect('/articles')->with('status', 'Une article a bien été ajouté avec succès.');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/articles', [ArticleController::class, 'liste_articles']);
+Route::get('/Add', [ArticleController::class, 'AddArticle']);
+Route::post('/Add/traitement', [ArticleController::class, 'traitementAdd']);
